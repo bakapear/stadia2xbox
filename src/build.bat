63503639bbd7d73a0821969826b5d1170616cb15
bakapear <proxybomb@gmail.com>
2022-01-19 23:11:18 +0100

    rename compile to build bat

---
+++ src/build.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %~dp0
+echo Compiling to ../build
+rsrc -manifest manifest.xml -ico data/stadia.ico
+go build -ldflags "-s -w -H windowsgui" -o ../build/stadia2xbox.exe
+copy data\ViGEmClient.dll ..\build\ViGEmClient.dll /y > NUL
+upx ../build/stadia2xbox.exe -9
--- src/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Compiling to ../build
-rsrc -manifest manifest.xml -ico data/stadia.ico
-go build -ldflags "-s -w -H windowsgui -linkmode=internal" -o ../build/stadia2xbox.exe
