b44cc234626ebbb10efbeb4459800c7e60653215
bakapear <proxybomb@gmail.com>
2022-01-20 13:52:08 +0100

    Remove upx because it requires more memory

--- src/build.bat
+++ src/build.bat
@@ -4,4 +4,3 @@ echo Compiling to ../build
 rsrc -manifest manifest.xml -ico data/stadia.ico
 go build -ldflags "-s -w -H windowsgui" -o ../build/stadia2xbox.exe
 copy data\ViGEmClient.dll ..\build\ViGEmClient.dll /y > NUL
-upx ../build/stadia2xbox.exe -9
